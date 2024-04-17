bdfcedface1167ad1bd7dd99aa9fcd3962d47407
Multyjog <simon.kotov.web@gmail.com>
2024-04-17 16:20:49 +0700

    - Downgrade to react query 4.28 - Added infinite query and dispalying logic

--- src/react-query/hooks/usePosts.ts
+++ src/react-query/hooks/usePosts.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 interface Post {
@@ -8,26 +8,28 @@ interface Post {
   userId: number;
 }
 interface PostQuery {
-  page: number;
   pageSize: number;
 }
 
 const usePosts = (query: PostQuery) => {
-  const fetchPosts = () =>
+  const fetchPosts = (pageParam: number) =>
     axios
       .get<Post[]>("https://jsonplaceholder.typicode.com/posts", {
         params: {
-          _start: (query.page - 1) * query.pageSize,
+          _start: (pageParam - 1) * query.pageSize,
           _limit: query.pageSize,
         },
       })
       .then((res) => res.data);
 
-  return useQuery<Post[]>({
+  return useInfiniteQuery<Post[], Error>({
     queryKey: ["posts", query],
-    queryFn: () => fetchPosts(),
+    queryFn: ({ pageParam = 1 }) => fetchPosts(pageParam),
     staleTime: 1 * 60 * 1000,
-    // TODO: keepPreviousData parameter is not specified
+    keepPreviousData: true,
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.length > 0 ? allPages.length + 1 : undefined;
+    },
   });
 };
 
